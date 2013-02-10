68422e661ef1a188edaa38c3fe99b5e90116717a
srosh <srosh@me.com>
2013-02-10 00:06:42 -0800

    index fix

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 var domarr = require('./dom.arr.js');
-var util = require('util');
 
-var Tag    = function (){}
-var DOM    = function (){
-	// switch from 
-	//   if
-	this.arr = domarr.makeDOM([])
-}
-
-var html   = function (){}
+var html   = require('./html.js')
 var emmet  = require('./emmet.js');
-var jsonml = function (){}
+var jsonml = require('./jsonml.js');
+var md     = require('./md.js');
 
 
 var select = require('./dom.select.js');
-exports.makeTag = domarr.makeTag;
-exports.makeDOM = domarr.makeDOM;
+
 exports.read    = {
 	html   : function () {},
 	emmet  : emmet.read,
-	jsonml : function () {}
+	jsonml : jsonml.read,
+	md     : md.read
 }
 exports.render  = {
 	html   : function (dom) {},
 	emmet  : emmet.render,
-	jsonml : function (dom) {}
+	jsonml : jsonml.render
 }
--- md.js
+++ md.js
@@ -3,7 +3,9 @@ var jml = require('./jsonml');
 var markdown = {}
 
 markdown.read = function (source) {
-	return jml.read(markdownobj.toHTMLTree(source));
+	var dom = jml.read(markdownobj.toHTMLTree(source));
+	dom.shift();
+	return dom;
 }
 
 markdown.render = null;
